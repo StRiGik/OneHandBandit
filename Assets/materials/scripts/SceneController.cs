9f430ed31c3862195642211a197dc676161d6192
StRiGik <vovastriga2003@gmail.com>
2024-02-06 16:27:00 +0300

    убрал рандомы для времени вращения следующих барабанов, тем самым они вращаются одинаковое кол-во времени

--- Assets/materials/scripts/SceneController.cs
+++ Assets/materials/scripts/SceneController.cs
@@ -24,23 +24,22 @@ public class SceneController : MonoBehaviour
     public void MethodOfRotationLevelArm()
     {
         seq = DOTween.Sequence();
-        seq.Append(obj.transform.DORotate(new Vector3(90, 0, 0), 0.8f, RotateMode.LocalAxisAdd).SetEase(Ease.Linear));
-        seq.Append(obj.transform.DORotate(new Vector3(-90, 0, 0), 0.2f, RotateMode.WorldAxisAdd).SetEase(Ease.Linear));
+        seq.Append(obj.transform.DORotate(new Vector3(50, 0, 0), 0.8f, RotateMode.LocalAxisAdd).SetEase(Ease.Linear));
+        seq.Append(obj.transform.DORotate(new Vector3(-50, 0, 0), 0.2f, RotateMode.WorldAxisAdd).SetEase(Ease.Linear));
         seq.SetEase(Ease.Linear);
         seq.AppendCallback(RollDrums);
     }
     private void RollDrums()
     {
-        randOfDegreeToNextSection = Random.Range(1000, 5000);
         randOfTimeRotation = Random.Range(3f, 10f);
+
+        randOfDegreeToNextSection = Random.Range(1000, 5000);
         drumOne.transform.DORotate(new Vector3(randOfDegreeToNextSection * degreeToNextSection, 0, 0), randOfTimeRotation, RotateMode.WorldAxisAdd).SetEase(Ease.InOutQuart);
 
         randOfDegreeToNextSection = Random.Range(1000, 5000);
-        randOfTimeRotation = Random.Range(3f, 10f);
         drumTwo.transform.DORotate(new Vector3(randOfDegreeToNextSection * degreeToNextSection, 0, 0), randOfTimeRotation, RotateMode.WorldAxisAdd).SetEase(Ease.InOutQuart).SetDelay(0.2f);
 
         randOfDegreeToNextSection = Random.Range(1000, 5000);
-        randOfTimeRotation = Random.Range(3f, 10f);
         drumThree.transform.DORotate(new Vector3(randOfDegreeToNextSection * degreeToNextSection, 0, 0), randOfTimeRotation, RotateMode.WorldAxisAdd).SetEase(Ease.InOutQuart).SetDelay(0.4f);
         
         Debug.Log("HIIII!");
